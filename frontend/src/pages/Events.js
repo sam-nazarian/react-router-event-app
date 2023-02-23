a5bcf8bdbc90f23f1055529de6ae4dd0e9360b16
samanfathnazarian <saman.nazarian17@gmail.com>
2023-02-23 16:29:10 -0600

    Returning Responses in loader()s

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -5,8 +5,8 @@ import EventsList from '../components/EventsList';
 
 // First renders component then after it fetches for the data,
 function EventsPage() {
-  const events = useLoaderData(); //gets access to closest loader data
-  // console.log(events);
+  const data = useLoaderData(); //gets access to closest loader data
+  const events = data.events;
 
   return <EventsList events={events} />;
 }
@@ -17,8 +17,12 @@ export async function loader() {
   if (!response.ok) {
     // ...
   } else {
-    const resData = await response.json();
-    return resData.events; //resData is an obj wtih events inside of it, events is an arr fo objects
+    // const resData = await response.json();
+    // return resData.events; //resData is an obj wtih events inside of it, events is an arr fo objects
+    // const res = new Response('any data', { status: 201 });
+    // return res; //data gets extracted from your responses when using useLoaderData()
+
+    return response; //fetch returns a promise that resolves to a response
   }
 }
 
