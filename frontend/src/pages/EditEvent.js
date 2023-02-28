d002bb74b2da272b8e5a08b40d1c4b0404759a1f
samanfathnazarian <saman.nazarian17@gmail.com>
2023-02-28 04:32:54 -0600

    Updated EditEvent.js Component To Use The Deferred Loader

--- frontend/src/pages/EditEvent.js
+++ frontend/src/pages/EditEvent.js
@@ -1,12 +1,20 @@
-import { useRouteLoaderData } from 'react-router-dom'; //useLoaderData
+import { Suspense } from 'react';
+import { useRouteLoaderData, Await } from 'react-router-dom'; //useLoaderData
 
 import EventForm from '../components/EventForm';
 
 function EventPage() {
   // const data = useLoaderData(); //highest it looks for is loader is it's current route definition
-  const data = useRouteLoaderData('event-detail'); //access data from a higher route
+  // const data = useRouteLoaderData('event-detail'); //access data from a higher route
+  // return <EventForm method="patch" event={data.event} />;
 
-  return <EventForm method="patch" event={data.event} />;
+  const { event } = useRouteLoaderData('event-detail');
+
+  return (
+    <Suspense fallback="Loading...">
+      <Await resolve={event}>{(loadEventData) => <EventForm event={loadEventData} method="PATCH" />}</Await>
+    </Suspense>
+  );
 }
 
 export default EventPage;
