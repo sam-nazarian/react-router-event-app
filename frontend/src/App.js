56c298e954802e07868717562809f955bcffcd15
samanfathnazarian <saman.nazarian17@gmail.com>
2023-02-27 18:00:04 -0600

    Implemented Newsletter With useFetcher()

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,5 @@
-// Challenge / Exercise
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import EditEventPage from './pages/EditEvent';
 import ErrorPage from './pages/Error';
 import EventDetailPage, { loader as eventDetailLoader, action as deleteEventAction } from './pages/EventDetail';
@@ -30,19 +9,15 @@ import HomePage from './pages/Home';
 import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
-// Just before elm gets rendered loader gets triggered
-// if a promise is returned (an async func returns a Promise) react-router will yield the value of the promise
 const router = createBrowserRouter([
   {
-    path: '/', //parent route
+    path: '/',
     element: <RootLayout />,
-    errorElement: <ErrorPage />, //react-router will display the closest error elm when an error gets thrown
+    errorElement: <ErrorPage />,
     children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
+      { index: true, element: <HomePage /> },
       {
         path: 'events',
         element: <EventsRootLayout />,
@@ -51,17 +26,16 @@ const router = createBrowserRouter([
           {
             path: ':eventId',
             id: 'event-detail',
-            // loader applied to current route level & levels below
-            loader: eventDetailLoader, // to get the data from the loader do: useRouteLoaderData('event-detail')
+            loader: eventDetailLoader,
             children: [
               { index: true, element: <EventDetailPage />, action: deleteEventAction },
               { path: 'edit', element: <EditEventPage />, action: manipulateEventAction },
             ],
           },
-          // as this is more specific react router will use the /new route, rather than '/:eventId' (order does not matter)
           { path: 'new', element: <NewEventPage />, action: manipulateEventAction },
         ],
       },
+      { path: 'newsletter', element: <NewsletterPage />, action: newsletterAction },
     ],
   },
 ]);
--- frontend/src/components/OldMainNavigation.js
+++ frontend/src/components/OldMainNavigation.js
@@ -1,7 +1,6 @@
 import { NavLink } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
-import NewsletterSignup from './NewsletterSignup';
 
 function MainNavigation() {
   return (
@@ -14,18 +13,13 @@ function MainNavigation() {
             </NavLink>
           </li>
           <li>
+            {/* always go to '/events' */}
             <NavLink to="/events" className={({ isActive }) => (isActive ? classes.active : undefined)}>
               Events
             </NavLink>
           </li>
-          <li>
-            <NavLink to="/newsletter" className={({ isActive }) => (isActive ? classes.active : undefined)}>
-              Newsletter
-            </NavLink>
-          </li>
         </ul>
       </nav>
-      <NewsletterSignup />
     </header>
   );
 }
